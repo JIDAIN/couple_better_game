3e477eb83544d4418531b9ba062d29f80162eb72
JIDAIN <43210783+JIDAIN@users.noreply.github.com>
2026-09-02 16:52:41 +0800

    V2-P5: add weight route

---
+++ app/nest/weight/page.tsx
@@ -0,0 +1,10 @@
+import { LifeAppShell } from "@/components/life/LifeAppShell";
+import { LifeWeightPage } from "@/components/life/LifeWeightPage";
+
+export default function WeightPage() {
+  return (
+    <LifeAppShell>
+      <LifeWeightPage />
+    </LifeAppShell>
+  );
+}
