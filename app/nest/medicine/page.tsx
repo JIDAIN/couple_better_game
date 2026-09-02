ad0921c29183dfcc7ab0846727dd706586a5462c
JIDAIN <43210783+JIDAIN@users.noreply.github.com>
2026-09-02 17:16:45 +0800

    V2-P6: route household medicine cabinet

---
+++ app/nest/medicine/page.tsx
@@ -0,0 +1,6 @@
+import { LifeAppShell } from "@/components/life/LifeAppShell";
+import { LifeMedicinePage } from "@/components/life/LifeMedicinePage";
+
+export default function MedicinePage() {
+  return <LifeAppShell><LifeMedicinePage /></LifeAppShell>;
+}
