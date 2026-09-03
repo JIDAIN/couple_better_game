aeb6088a9f8335d308c660f316f7e96646cbb423
JIDAIN <43210783+JIDAIN@users.noreply.github.com>
2026-09-03 18:26:11 +0800

    R8.2: add data management route

---
+++ app/me/data/page.tsx
@@ -0,0 +1,10 @@
+import { LifeAppShell } from "@/components/life/LifeAppShell";
+import { LifeDataManagementPage } from "@/components/life/LifeDataManagementPage";
+
+export default function DataManagementPage() {
+  return (
+    <LifeAppShell>
+      <LifeDataManagementPage />
+    </LifeAppShell>
+  );
+}
