ce152b0d461dfe892009b028d9cba994175c06cf
JIDAIN <43210783+JIDAIN@users.noreply.github.com>
2026-09-07 08:08:30 +0800

    feat(reminders): add reminder center page

---
+++ app/me/reminders/page.tsx
@@ -0,0 +1,3 @@
+import { LifeAppShell } from "@/components/life/LifeAppShell";
+import { LifeReminderCenterPage } from "@/components/life/LifeReminderCenterPage";
+export default function ReminderCenterPage(){return <LifeAppShell><LifeReminderCenterPage/></LifeAppShell>;}
